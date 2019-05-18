5ccb8bc2f94a9dbd51292df6e1ae09e427afea8a
Zach McWhirter <zach@brians-mbp-3.attlocal.net>
2019-05-18 15:43:57 -0500

    add button and label.  Make them toggle

--- TestApp/TestApp/ViewController.swift
+++ TestApp/TestApp/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var theLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressMeTapped(_ sender: Any) {
+        if theLabel.text == "I Rock" {
+            theLabel.text = "I Don't Rock"
+        } else {
+            theLabel.text = "I Rock"
+        }
+    }
+    
 }
 
